35b856d5a9dbb2cc1c87b83db11eb06b0e0c7ec5
Erik Olsen <erik.s.olsen@gmail.com>
2015-06-28 15:33:54 -0500

    Add store front and style

--- spec/views/products/index.html.haml_spec.rb
+++ spec/views/products/index.html.haml_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe "products/index", :type => :view do
   before(:each) do
     assign(:products, [
       Product.create!(
-        :title => "Title",
+        :title => "SomeTitle",
         :description => "MyText",
-        :image_url => "Image Url",
+        :image_url => "MyImage.jpg",
         :price => "9.99"
       ),
       Product.create!(
-        :title => "Title",
+        :title => "SomeOtherTitle",
         :description => "MyText",
-        :image_url => "Image Url",
+        :image_url => "MyImage.jpg",
         :price => "9.99"
       )
     ])
@@ -20,9 +20,9 @@ RSpec.describe "products/index", :type => :view do
 
   it "renders a list of products" do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "SomeTitle".to_s, :count => 1
+    assert_select "tr>td", :text => "SomeOtherTitle".to_s, :count => 1
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Image Url".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+    assert_select "tr>td", :text => "MyImage.jpg".to_s, :count => 2
   end
 end
